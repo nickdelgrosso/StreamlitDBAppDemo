1aa42a409ba3549a6008f64d11ab2f3e449db8b7
Nick A. Del Grosso <delgrosso.nick@gmail.com>
2023-06-12 11:43:30 +0200

    updated sqlalchemy code to 2.0, included join

--- repos/sqlilte_repo.py
+++ repos/sqlilte_repo.py
@@ -1,9 +1,16 @@
+import contextlib
 from datetime import datetime
+import os
 from typing import Optional
 from uuid import uuid4
 import pandas as pd
-from sqlalchemy import create_engine, MetaData, Table, Column, String, Integer, UUID, DateTime
-from repos.base import IPatientRepo
+from sqlalchemy import Text, create_engine, MetaData, Table, Column, String, Integer, UUID, DateTime, delete, insert, join, select, text
+
+
+try:
+    from repos.base import IPatientRepo
+except:
+    from base import IPatientRepo
 
 
                 
@@ -21,6 +28,13 @@ class SqlitePatientRepo(IPatientRepo):
             Column('name', String),
             Column('age', Integer),
         )
+        
+        self.sessions_table = Table('sessions', metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('patient_id', String),
+            Column('comment', String),
+        )
+        
         self.metadata = metadata
         self.conn = self.engine.connect()
         
@@ -28,7 +42,8 @@ class SqlitePatientRepo(IPatientRepo):
         self.metadata.create_all(self.engine)
     
     def add_patient(self, name: str, age: int) -> None:
-        self.conn.execute(self.patients_table.insert(), dict(id=str(uuid4()), created_on=datetime.now(), name=name, age=age))
+        query = insert(self.patients_table).values(id=str(uuid4()), created_on=datetime.now(), name=name, age=age)
+        self.conn.execute(query)
         self.conn.commit()
         
     def get_all_patients(self) -> pd.DataFrame | None:
@@ -36,25 +51,63 @@ class SqlitePatientRepo(IPatientRepo):
 
     
     def archive_patient(self, id: str) -> None:
-        patients = self.patients_table
-        self.conn.execute(patients.delete().where(patients.c.id == id))
+        query = delete(self.patients_table).where(self.patients_table.c.id == id)
+        self.conn.execute(query)
+        self.conn.commit()
+        
+    def add_session(self, patient_id: str, comment: str) -> None:
+        query = insert(self.sessions_table).values(patient_id=patient_id, comment=comment)
+        self.conn.execute(query)
         self.conn.commit()
     
+    def get_all_sessions(self) -> pd.DataFrame:
+        return pd.read_sql_table('sessions', con=self.conn)
+        
     
+    def get_full_sessions(self) -> pd.DataFrame:
+        stmt = (
+        select(
+            self.sessions_table.c.id,
+            self.patients_table.c.name, 
+            self.patients_table.c.age, 
+            self.sessions_table.c.comment,
+        )
+        .join_from(
+            self.patients_table, 
+            self.sessions_table, 
+            self.patients_table.c.id == self.sessions_table.c.patient_id
+        )
+        )
+        
+        return pd.read_sql_query(stmt, con=self.conn)
+        
+    #   stmt = select(user_table, address_table).join_from(
+    #     user_table, address_table, user_table.c.id == address_table.c.user_id
     
 if __name__ == '__main__':
-    repo = SqlitePatientRepo(path='patients.db')
+    
+    with contextlib.suppress():
+        os.remove('test2.db')
+    
+    repo = SqlitePatientRepo(path='test2.db')
+    repo.create_db()
     print("Before Run:")
-    print(repo.get_all_patients())
+    print(repo.get_all_patients(), end='\n\n')
     
     print("Adding another patient:")
     repo.add_patient(name='Nick', age=35)
     df = repo.get_all_patients()
-    print(df)
+    print(df, end='\n\n')
     
-    print("Removing the last patient:")
+    # print("Removing the last patient:")
     last_patient = df.iloc[-1]
-    repo.archive_patient(last_patient.id)
-    print(repo.get_all_patients())
+    # repo.archive_patient(last_patient.id)
+    # print(repo.get_all_patients(), end='\n\n')
     
-    repo.archive_patient
+    
+    repo.add_session(patient_id=last_patient.id, comment="Sick.  I don't know why...")
+    repo.add_session(patient_id=last_patient.id, comment="Healthy!  I don't know why...")
+    print(repo.get_all_sessions(), end='\n\n')
+    
+    print(repo.get_full_sessions(), end='\n\n')
+    
